b0c0039b50b26d256f5bc8e13dc7e7a9fa22017c
Surya Mani Verma <93043048+suryamani25@users.noreply.github.com>
2025-06-30 22:56:37 +0530

    Palindrome Check Any Number

---
+++ 3_PalindromeCheck.cpp
@@ -0,0 +1,28 @@
+ #include <iostream>
+ using namespace std;
+
+ int main() {
+
+ int num, reversedNum = 0, remainder, originalNum;
+
+ cout << "Enter an integer: ";
+ cin >> num;
+
+ originalNum = num; 
+
+ while (num != 0) {
+
+ remainder = num % 10;
+
+ reversedNum = reversedNum * 10 + remainder;
+ 
+ num /= 10;
+ }
+
+ if (originalNum == reversedNum) {
+ cout << originalNum << " is a palindrome." << endl;
+ } else {
+ cout << originalNum << " is not a palindrome." << endl;
+ }
+ return 0;
+ }
